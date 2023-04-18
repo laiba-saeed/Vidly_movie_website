572b838c743a8daaf9fe67461560de2ee8428905
Laiba <laibasaeed538@gmail.com>
2023-04-18 15:56:46 +0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var _ = require('underscore');
+const config = require('config');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const auth = require('./routes/auth');
+const app = express();
 
-var result = _.conatins([1, 2, 4], 2);
-console.log(result);
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
+
+// mongodb connection here
+// .....
+
+app.use(express.json());
+app.use('/api/users', users);
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
+app.use('/api/auth', auth)
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Listening to the ${port}`));
